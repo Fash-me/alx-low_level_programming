0282e9cf7dc35ed81b45e0ab9eed3adea634a7a7
Fash-me <fasujitosin@gmail.com>
2022-07-01 16:51:07 -0700

    Without prinf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-
-#include <stdio.h>                                                                                                                              
-                                                                                                                                                
-/**                                                                                                                                             
- *  * main - Entry point                                                                                                                           
- *   *                                                                                                                                              
- *    * Return: Always 1 (Success)                                                                                                                   
- *     */                                                                                                                                             
-int main(void)                                                                                                                                  
-{                                                                                                                                               
-	        const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	syscall(SYS_write, 1, msg, size(msg));
-	return (1);	
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char msg[] = "and that piece of art is useful\” - Dora Korpar, 2015-10-19";
+write(2, msg, 59);
+return (0);
+}
