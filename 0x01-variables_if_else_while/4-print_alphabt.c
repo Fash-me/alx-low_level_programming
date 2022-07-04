c751410457140195a0fc4dd258ed644d350354d5
Fash-me <fasujitosin@gmail.com>
2022-07-04 11:28:17 -0700

    Except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (n == 'e')
+		{
+			continue;
+		} else if (n == 'q')
+		{
+			continue;
+		}
+
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
