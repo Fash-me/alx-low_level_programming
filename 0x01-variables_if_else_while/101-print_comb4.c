95a03c87cd88034c3e8f4ba5c45a4ce27f2ff17e
Fash-me <fasujitosin@gmail.com>
2022-07-04 17:14:45 -0700

    More number

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, i, m;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (m = '0'; m <= '9'; m++)
+			{
+				if (n < i && i < m)
+				{
+					putchar(n);
+					putchar(i);
+
+					if (n != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
