b5471d6198b6abfaab51d799ff09d3da7c50ed59
Fash-me <fasujitosin@gmail.com>
2022-07-04 17:46:03 -0700

    Final task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, i, m, j;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (m = 48; m <= 57; m++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+					if (((m + j)  > (n + i) && m >= n) || n < m)
+					{
+						putchar(n);
+						putchar(i);
+						putchar(' ');
+						putchar(m);
+					if (n + i + m + j == 227 && n == 57)
+					{
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
