7cbfc57742223d7aaff9467fc46f3dbb275bb035
Fash-me <fasujitosin@gmail.com>
2022-07-06 17:16:38 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit
+ * @n: integer
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	int digit = n % 10;
+
+	if (digit < 0)
+	{
+		digit *= -1;
+	}
+	_putchar(digit + '0');
+	return (digit);
+}
