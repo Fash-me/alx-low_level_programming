d2a1f01f9f54486eef2efb484f093c90fcbeeae3
Fash-me <fasujitosin@gmail.com>
2022-07-12 12:32:13 -0700

    Pointer function first example

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that change the value of a pointer
+ * @n: parameter
+ * Return: null
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+/**
+ * main - header for prototypes
+ * void  prototype()
+ * int prototype()
+ * char prototype()
+ */
+
+void reset_to_98(int *n);
