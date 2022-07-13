b1f473cb0028dce544e09b9b0524cf2ea8ae5819
Fash-me <fasujitosin@gmail.com>
2022-07-12 20:31:49 -0700

    Elemts with n value

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_array - print array
+*@a: array
+*@n: number of elements
+*Return: return nill
+*/
+void print_array(int *a, int n)
+{
+int inc;
+for (inc = 0; inc < n ; inc++)
+{
+if (inc != n - 1)
+printf("%d, ", a[inc]);
+else
+printf("%d", a[inc]);
+
+}
+printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,3 +15,4 @@ void rev_string(char *s);
 void swap_char(char *a, char *b);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
