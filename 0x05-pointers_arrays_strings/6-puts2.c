fdeec0a98aff085646a0ced5021cf4a9ff369487
Fash-me <fasujitosin@gmail.com>
2022-07-12 20:00:21 -0700

    Other numbers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - Show the first number and other numbers
+ * @str; string
+ * Return: return nill
+ */
+void puts2(char *str)
+{
+int n = 0;
+
+while (str[n] != '\0')
+{
+	if (n % 2 == 0)
+	{
+		_putchar(str[n]);
+	}
+	n++;
+}
+_putchar('\n');
+}
