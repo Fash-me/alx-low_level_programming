00c753e0f3e7deeae5a5cff32c3fd59a14984f16
Fash-me <fasujitosin@gmail.com>
2022-07-14 09:53:39 -0700

    Reverse array of numbers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*  reverse_array - Reverse numbers
+*  @a: array
+*  @n: number of elements
+*/
+void reverse_array(int *a, int n)
+{
+int tmp, i;
+for (i = n - 1; i > n / 2; i--)
+{
+tmp = a[n - 1 - i];
+a[n - 1 - i] = a[i];
+a[i] = tmp;
+}
+}
