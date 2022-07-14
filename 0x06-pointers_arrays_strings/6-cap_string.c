8b73c67423419d41899ef1a14fd08074eba7b976
Fash-me <fasujitosin@gmail.com>
2022-07-14 11:35:10 -0700

    Updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,32 @@
 #include "main.h"
 /**
- *cap_string - Capitalize the first character of a string
+ *cap_string- converts Initial lowercase to uppercase
  *@str: string
- *Return:return str
+ *Return: return str
  */
 char *cap_string(char *str)
 {
-int i = 0;
-while (str[++i])
-{
-while (!(str[i] >= 'a' && str[i] <= 'z'))
-i++;
+	int i, j, s;
 
-if (str[i - 1] == ' ' ||
-	str[i - 1] == '\t' ||
-	str[i - 1] == '\n' ||
-	str[i - 1] == ',' ||
-	str[i - 1] == ';' ||
-	str[i - 1] == '.' ||
-	str[i - 1] == '!' ||
-	str[i - 1] == '?' ||
-	str[i - 1] == '"' ||
-	str[i - 1] == '(' ||
-	str[i - 1] == ')' ||
-	str[i - 1] == '{' ||
-	str[i - 1] == '}')
-str[i] -= 32;
-}
-return (str);
+	s = 1;
+
+	for (i = 0; str[i]; ++i)
+	{
+		if (s && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] -  32;
+		}
+		s = 0;
+		for (j = 0; j < 12; j++)
+		{
+			if (str[i] == '\t' || str[i] == '\n' || str[i] == ','
+					|| str[i] == '\"' || str[i] == '.' || str[i] == '!' ||
+					str[i] == '{' || str[i] == '}' || str[i] == '(' ||
+					str[i] == ')' || str[i] == ' ' || str[i] == '?')
+			{
+				s = 1;
+			}
+		}
+	}
+	return (str);
 }
