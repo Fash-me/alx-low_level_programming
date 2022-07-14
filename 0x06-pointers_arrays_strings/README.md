958c74894763eb6253453ce60ccce2fa17e73835
Fash-me <fasujitosin@gmail.com>
2022-07-14 08:57:32 -0700

    String concatenation

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Readme file for more pointers, array and strings
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+/**
+ *main - header for prototypes
+ *void  prototype()
+ *int prototype()
+ *char prototype()
+ */
+char *_strcat(char *dest, char *src);
