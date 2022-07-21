e9c91dd9997db24e3e50f9bf5a53b203629ebf18
Fash-me <fasujitosin@gmail.com>
2022-07-21 13:40:33 -0700

    Print string with new line

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stddef.h>
+/**
+ *main - header for prototypes
+ *void  prototype()
+ *int prototype()
+ *char prototype()
+ */
+int _putchar(char c);
+void _puts_recursion(char *s);
