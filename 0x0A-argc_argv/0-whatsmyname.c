c809b3a60e1d8078cae9be9c8c97bf461b2dc08e
Fash-me <fasujitosin@gmail.com>
2022-07-23 11:23:47 -0700

    Print name followed by new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - Function
+*@argc: command line count
+*@argv: command line value
+*Return: return 0
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - 1]);
+return (0);
+}
