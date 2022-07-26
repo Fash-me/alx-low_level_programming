b0e4290486673019e02c9e85372ab37bf97f07e8
Fash-me <fasujitosin@gmail.com>
2022-07-26 14:03:15 -0700

    Create array

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stddef.h>
+/**
+ *main - header for prototypes
+ *void  prototype()
+ *int prototype()
+ *char prototype()
+ */
+
+char *create_array(unsigned int size, char c);
