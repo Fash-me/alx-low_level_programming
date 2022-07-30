d5f61d93027819615ffc6a61785ae34d04c8f2aa
Fash-me <fasujitosin@gmail.com>
2022-07-29 18:08:47 -0700

    multiplies two positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,3 +13,13 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #define min(x, y) (((x) < (y)) ? (x) : (y))
 
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+char *iterate_zeroes(char *str);
+int get_digit(char c);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+int main(int argc, char *argv[]);
+
+int _putchar(char c);
