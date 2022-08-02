e360779231d5f285936443b7a3265c4c77000556
Fash-me <fasujitosin@gmail.com>
2022-08-02 09:41:25 -0700

    Struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - dog profil
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog's owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
